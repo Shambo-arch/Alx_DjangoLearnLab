c998c6db1358cd17f4f27a6a96d4e9b60bf17585
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-12-01 12:16:48 -0800

    added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
-
-
-# Create your views here.
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
