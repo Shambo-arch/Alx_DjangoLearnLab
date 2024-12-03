632ca8076ce5032dde6b75cc8e782df0cf66ecba
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-12-03 09:27:13 -0800

    add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,3 @@
-from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from django_filters import rest_framework
-
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
@@ -33,66 +24,3 @@ class BookListView(generics.ListAPIView):
     search_fields = ['title', 'author']  # Fields to search
     ordering_fields = ['title', 'publication_year']  # Fields to order by
     ordering = ['title']  # Default ordering
-
-
-
-
-
-
-
-# Create your views here.
-class BookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-# DetailView for retrieving a single book by ID
-class BookDetailView(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-class BookCreateView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        # Example: Automatically assign the currently logged-in user as the book creator
-        serializer.save(created_by=self.request.user)
-
-    def create(self, request, *args, **kwargs):
-        # Custom response handling
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(
-            {"message": "Book created successfully!", "data": serializer.data},
-            status=status.HTTP_201_CREATED
-        )
-
-# UpdateView for modifying an existing book
-class BookUpdateView(generics.UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-    def update(self, request, *args, **kwargs):
-        # Custom response handling
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(
-            {"message": "Book updated successfully!", "data": serializer.data},
-            status=status.HTTP_200_OK
-        )
-
-# DeleteView for removing a book
-class BookDeleteView(generics.DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-
