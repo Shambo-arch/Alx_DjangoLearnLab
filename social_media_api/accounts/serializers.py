57b346d5472bf4bbadf8cb2f3de0d2642b421e40
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-12-10 03:40:48 -0800

    added

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+User = get_user_model()  # Dynamically get the user model (default or custom)
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    # Explicitly define fields using serializers.CharField
+    username = serializers.CharField(max_length=150)
+    email = serializers.CharField(max_length=254)
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     confirm_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
@@ -13,21 +15,27 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'confirm_password']
 
     def validate(self, data):
-        # Check if passwords match
+        """
+        Check that the passwords match.
+        """
         if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError("Passwords do not match.")
+            raise serializers.ValidationError({"password": "Passwords do not match."})
         return data
 
     def create(self, validated_data):
-        # Remove confirm_password before creating the user
-        validated_data.pop('confirm_password')
-        user = User.objects.create_user(**validated_data)
-        # Create an authentication token for the user
-        Token.objects.create(user=user)
+        """
+        Create a user instance after validating and removing unnecessary fields.
+        """
+        validated_data.pop('confirm_password')  # Remove confirm_password before creating the user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
 
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
+    username = serializers.CharField(max_length=150)
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
