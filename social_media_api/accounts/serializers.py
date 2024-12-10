3483749c7983e0a46ac5cfdba72b9e092e74e76a
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-12-10 04:01:47 -0800

    added

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()  # Dynamically get the user model (default or custom)
 
