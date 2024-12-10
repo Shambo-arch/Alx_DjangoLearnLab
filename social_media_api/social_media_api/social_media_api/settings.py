6921e04119d137c2d5ea0d45ff5e96f060390a7d
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-12-10 02:45:43 -0800

    added

--- social_media_api/social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/social_media_api/settings.py
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'social_media_api'
     'rest_framework.authtoken',
-    'accounts.apps.AccountsConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
