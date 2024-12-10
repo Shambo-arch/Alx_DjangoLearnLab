ff45b83c73b8eaaccca621bca39fa2a85819ed4c
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-12-10 02:41:09 -0800

    added social media app

---
+++ social_media_api/social_media_api/accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
